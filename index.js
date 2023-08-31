f23eea6f89365ef0a8947e18d68f0c27266e6972
gangz <gangz2009@gmail.com>
2023-08-31 20:23:28 +0800

    add refresh token EndPoint

--- index.js
+++ index.js
@@ -2,35 +2,59 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('./logger')
 const getSessionInfoFromWeixin = require('./weixinAuth');
-const keycloakAuth = require('./keycloakAuth');
+const {keycloakAuth,keycloakRefreshToken} = require('./keycloakAuth');
 const { PORT } = require('./config');
 
 
 const app = express();
 app.use(bodyParser.json());
 
+/**
+ * handle Login Request
+ * @param {*} req 
+ * @param {*} res 
+ */
 async function handleLoginRequest(req, res) {
   const code = req.body.code;
   logger.info(`login received : ${code}`);
 
   try {
-      const { openid } = await getSessionInfoFromWeixin(code);
-      logger.info(`login successful with openid: ${openid}`);
-      const token = await generateToken(openid); 
-      // 返回token给小程序端
-      res.json({ token });
+    const { openid } = await getSessionInfoFromWeixin(code);
+    logger.info(`login successful with openid: ${openid}`);
+    const token = await generateToken(openid);
+    // 返回token给小程序端
+    res.send(token);
   } catch (error) {
-      logger.error(error.message);
-      res.status(500).send('登录失败');
+    logger.error(error.message);
+    res.status(500).send('登录失败');
   }
 }
+
 async function generateToken(openid) {
   const authResult = await keycloakAuth(openid);
-  return authResult.access_token;
+  return authResult;
 }
+
+
 
 app.post('/login', handleLoginRequest);
 
+async function handleRefreshTokenRequest(req, res) {
+  const refreshToken = req.body.refresh_token;
+  if (!refreshToken) {
+    return res.status(400).json({ error: 'Refresh token is required' });
+  }
+
+  try {
+    const token = await keycloakRefreshToken(refreshToken);
+    res.send(token);
+  } catch (error) {
+    logger.error(error.message);
+    res.status(500).json({ error: 'Failed to refresh token' });
+  }
+}
+
+app.post('/refresh-token', handleRefreshTokenRequest);
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
--- keycloakAuth.js
+++ keycloakAuth.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
 const KeycloakAdminClient = require('keycloak-admin').default;
-const qs = require('qs'); 
-const {KEYCLOAK_ADMIN_CLIENT_ID, 
+const qs = require('qs');
+const { KEYCLOAK_ADMIN_CLIENT_ID,
     KEYCLOAK_ADMIN_PASSWORD,
-     KEYCLOAK_ADMIN_USERNAME, 
-     KEYCLOAK_AUTH_CLIENT_ID, 
-     KEYCLOAK_BASE_URL, 
-     KEYCLOAK_NEW_USER_DEFAULT_PASSWORD, 
-     KEYCLOAK_REALM_NAME} = require('./config');
+    KEYCLOAK_ADMIN_USERNAME,
+    KEYCLOAK_AUTH_CLIENT_ID,
+    KEYCLOAK_BASE_URL,
+    KEYCLOAK_NEW_USER_DEFAULT_PASSWORD,
+    KEYCLOAK_REALM_NAME } = require('./config');
 
 const keycloakConfig = {
-    baseUrl: KEYCLOAK_BASE_URL, 
+    baseUrl: KEYCLOAK_BASE_URL,
     realmName: KEYCLOAK_REALM_NAME,
 };
 
@@ -33,25 +33,45 @@ async function keycloakAuth(openid) {
         await keycloakAdmin.users.create({
             username: openid,
             enabled: true,
-            credentials: [{ type: 'password', 
-            value: KEYCLOAK_NEW_USER_DEFAULT_PASSWORD }] 
+            credentials: [{
+                type: 'password',
+                value: KEYCLOAK_NEW_USER_DEFAULT_PASSWORD
+            }]
         });
     }
 
-    let data  = {
+    let data = {
         username: openid,
         password: KEYCLOAK_NEW_USER_DEFAULT_PASSWORD,
         client_id: KEYCLOAK_AUTH_CLIENT_ID,
         grant_type: 'password'
     };
     // 从Keycloak获取token
-    const tokenResponse = await axios.post(
-        `${keycloakConfig.baseUrl}/realms/${keycloakConfig.realmName}/protocol/openid-connect/token`, 
+    const keycloakResponse = await axios.post(
+        `${keycloakConfig.baseUrl}/realms/${keycloakConfig.realmName}/protocol/openid-connect/token`,
         qs.stringify(data),
-        {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
-        );
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    );
 
-    return tokenResponse.data;
+    return keycloakResponse.data;
 }
 
-module.exports = keycloakAuth;
+async function keycloakRefreshToken(refreshToken) {
+
+    let data = {
+        client_id: KEYCLOAK_AUTH_CLIENT_ID,
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+    };
+    const keycloakResponse = await axios.post(
+        `${keycloakConfig.baseUrl}/realms/${keycloakConfig.realmName}/protocol/openid-connect/token`,
+        qs.stringify(data),
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    );
+    return keycloakResponse.data;
+}
+
+module.exports = {
+    keycloakAuth,
+    keycloakRefreshToken
+};
